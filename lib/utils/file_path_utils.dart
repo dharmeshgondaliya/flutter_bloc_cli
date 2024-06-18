3299e748f3b0b41d13786231a9fad404bc38929a
Hiren Padaliya <hirenp961@gmail.com>
2024-06-18 12:51:21 +0530

    optimized filepath extension

--- lib/utils/file_path_utils.dart
+++ lib/utils/file_path_utils.dart
@@ -2,21 +2,16 @@ import 'dart:io';
 
 extension FilePath on String {
   String actualPath() {
-     String path = this;
-     if (path.contains('\\')) {
-       if (Platform.isLinux || Platform.isMacOS) {
-         return path.replaceAll('\\', '/');
-       } else {
-         return path;
-       }
-     } else if (path.contains('/')) {
-       if (Platform.isWindows) {
-         return path.replaceAll('/', '\\\\');
-       } else {
-         return path;
-       }
-     } else {
-       return path;
-     }
+    // If the string is empty, return it as is
+    if (isEmpty) return this;
+
+    // Normalize backslashes and slashes based on the current platform
+    if (Platform.isWindows) {
+      // Convert slashes to backslashes for Windows
+      return replaceAll('/', '\\');
+    } else {
+      // Convert backslashes to slashes for Unix-based systems
+      return replaceAll('\\', '/');
+    }
   }
 }
