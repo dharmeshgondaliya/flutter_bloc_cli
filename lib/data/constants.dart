dc4eef3ab517ae65db7170caca1f7518a88bf32f
Dharmesh Gondaliya <gondaliyadharmesh2@gmail.com>
2024-06-23 18:56:40 +0530

    change log updated

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 abstract class Constants {
-
   // Directories
   static final String appDirectoryPath = "/lib/App";
   static final String dataDirectoryPath = "/lib/App/data";
@@ -16,28 +13,44 @@ abstract class Constants {
   static final String appFilePath = "/lib/app.dart";
   static final String pubspecFilePath = "/pubspec.yaml";
   static final String appRoutesPath = "/lib/App/routes/app_routes.dart";
-  static final String routeNavigatorPath = "/lib/App/routes/route_navigator.dart";
+  static final String routeNavigatorPath =
+      "/lib/App/routes/route_navigator.dart";
 
   // Constants
-  static final String colorConstantsPath = "/lib/App/data/constants/color_constants.dart";
-  static final String urlManagerPath = "/lib/App/data/constants/url_manager.dart";
+  static final String colorConstantsPath =
+      "/lib/App/data/constants/color_constants.dart";
+  static final String urlManagerPath =
+      "/lib/App/data/constants/url_manager.dart";
 
   // Providers
-  static final String preferenceProviderPath = "/lib/App/data/provider/preference_provider.dart";
-  static final String apiProviderPath = "/lib/App/data/provider/api_provider.dart";
+  static final String preferenceProviderPath =
+      "/lib/App/data/provider/preference_provider.dart";
+  static final String apiProviderPath =
+      "/lib/App/data/provider/api_provider.dart";
 
   // Screens
-  static final String baseScreenPath = "/lib/App/screens/base_screen/view/base_screen.dart";
-  static final String baseDialogPath = "/lib/App/screens/base_screen/view/base_dialog.dart";
-  static final String customAppbarPath = "/lib/App/screens/base_screen/view/custom_appbar.dart";
-  static final String splashScreenPath = "/lib/App/screens/splash_screen/view/splash_screen.dart";
-  static final String homeScreenPath = "/lib/App/screens/home_screen/view/home_screen.dart";
-  static final String homeScreenRepositoryPath = "/lib/App/screens/home_screen/repository/home_screen_repository.dart";
-  static final String homeScreenBlocPath = "/lib/App/screens/home_screen/bloc/home_screen_bloc.dart";
-  static final String homeScreenStatePathBloc = "/lib/App/screens/home_screen/bloc/home_screen_state.dart";
-  static final String homeScreenEventPath = "/lib/App/screens/home_screen/bloc/home_screen_event.dart";
-  static final String homeScreenCubitPath = "/lib/App/screens/home_screen/cubit/home_screen_cubit.dart";
-  static final String homeScreenStatePathCubit = "/lib/App/screens/home_screen/cubit/home_screen_state.dart";
+  static final String baseScreenPath =
+      "/lib/App/screens/base_screen/view/base_screen.dart";
+  static final String baseDialogPath =
+      "/lib/App/screens/base_screen/view/base_dialog.dart";
+  static final String customAppbarPath =
+      "/lib/App/screens/base_screen/view/custom_appbar.dart";
+  static final String splashScreenPath =
+      "/lib/App/screens/splash_screen/view/splash_screen.dart";
+  static final String homeScreenPath =
+      "/lib/App/screens/home_screen/view/home_screen.dart";
+  static final String homeScreenRepositoryPath =
+      "/lib/App/screens/home_screen/repository/home_screen_repository.dart";
+  static final String homeScreenBlocPath =
+      "/lib/App/screens/home_screen/bloc/home_screen_bloc.dart";
+  static final String homeScreenStatePathBloc =
+      "/lib/App/screens/home_screen/bloc/home_screen_state.dart";
+  static final String homeScreenEventPath =
+      "/lib/App/screens/home_screen/bloc/home_screen_event.dart";
+  static final String homeScreenCubitPath =
+      "/lib/App/screens/home_screen/cubit/home_screen_cubit.dart";
+  static final String homeScreenStatePathCubit =
+      "/lib/App/screens/home_screen/cubit/home_screen_state.dart";
 
   // Utils
   static final String commonUtilsFilePath = "/lib/App/utils/common.dart";
@@ -45,8 +58,11 @@ abstract class Constants {
   static final String textStyleFilePath = "/lib/App/utils/app_text_style.dart";
 
   // Widgets
-  static final String textFieldWidgetPath = "/lib/App/widgets/app_textfield.dart";
+  static final String textFieldWidgetPath =
+      "/lib/App/widgets/app_textfield.dart";
   static final String checkboxWidgetPath = "/lib/App/widgets/app_checkbox.dart";
-  static final String radioButtonWidgetPath = "/lib/App/widgets/app_radio_button.dart";
-  static final String networkImageWidgetPath = "/lib/App/widgets/app_network_image.dart";
+  static final String radioButtonWidgetPath =
+      "/lib/App/widgets/app_radio_button.dart";
+  static final String networkImageWidgetPath =
+      "/lib/App/widgets/app_network_image.dart";
 }
